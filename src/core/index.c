80c27cd1126cca7d1a5825ecb9fdb61a8099f5cf
Greivin <sgandres21@gmail.com>
2022-05-26 17:01:47 -0400

    Logout /reload bux fig

--- src/core/index.c
+++ src/core/index.c
@@ -12,6 +12,19 @@ int main(int argc, char **argv, char **env){
     box_send_headers(http);
     box_send_html(http);
 
+    if(box_http_has_post(http)==1){
+        char * logout = box_post_param(http,"logout");
+        if (logout == NULL){
+            MYSQL *connection= init_sql_connection();
+            box_token * token_id = box_get_token(http);
+            box_user * user = sql_get_user_by_token(connection,token_id);
+            box_clear_token(user);
+            sql_save_user(connection,user);
+            close_sql_connection(connection);
+            box_http_redirect(http,"index.cgi");
+        }
+    }
+
     box_destroy_http(http);
 
     return 0;
