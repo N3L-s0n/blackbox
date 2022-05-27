0f2fe40e1c65c86aa7226a1bac794d7d4511df8a
N3L-s0n <nelson.alvarezcedeno@gmail.com>
2022-05-26 23:12:42 -0400

    Logout working

--- src/core/index.c
+++ src/core/index.c
@@ -3,8 +3,6 @@
 int main(int argc, char **argv, char **env){
     
     box_http *http = box_new_http("../html/index.html", env);
-    /*sls*/
-
     box_http_content(http, 2, TEXT_HTML, CHARSET_UTF_8);
 
     box_set_class_variables(http, "subheader", "subtitle=Aiming for secure.", 0);
@@ -12,20 +10,6 @@ int main(int argc, char **argv, char **env){
     box_send_headers(http);
     box_send_html(http);
 
-    if(box_http_has_post(http)==1){
-        char * logout = box_post_param(http,"logout");
-        if (logout == NULL){
-            MYSQL *connection= init_sql_connection();
-            box_token * token_id = box_get_token(http);
-            box_user * user = sql_get_user_by_token(connection,token_id);
-            box_clear_token(user);
-            sql_save_user(connection,user);
-            close_sql_connection(connection);
-            box_http_redirect(http,"index.cgi");
-        }
-    }
-
     box_destroy_http(http);
-
     return 0;
 }
